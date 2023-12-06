4dc1d2e076f41bf5fd77c5e904fa3ceac219f39b
Spikey <rafal.paciorek19@gmail.com>
2023-12-06 23:08:26 +0100

    add form for creation of user

--- rent_flat/authSystem/admin.py
+++ rent_flat/authSystem/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "username", "password1", "password2", "is_staff",
+                "is_active", "user_permissions"
+            )}
+         ),
+    )
+    list_display = ("email", "username", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
