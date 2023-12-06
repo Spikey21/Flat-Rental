59baa7c9b1448ccb0c0be29625e78be3c4a45cc0
Spikey <rafal.paciorek19@gmail.com>
2023-12-06 23:03:26 +0100

    created user and user manager model

--- rent_flat/authSystem/models.py
+++ rent_flat/authSystem/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have an username')
+
+        now = timezone.now()
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            is_active=True,
+            date_joined=now,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password, **extra_fields):
+        user = self._create_user(
+            email,
+            username,
+            password,
+            **extra_fields
+        )
+        return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        user = self._create_user(
+            email,
+            username,
+            password,
+            is_staff=True,
+            is_superuser=True,
+            **extra_fields
+        )
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return f'Email: {self.email} ' \
+               f'Username: {self.username}'
+
+    def get_absolute_url(self):
+        return reverse('SignUp', kwargs=self.pk)
