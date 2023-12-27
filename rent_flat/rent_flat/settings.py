44eaa506af9cc947fd8948b417a295bafaee7b02
Spikey <rafal.paciorek19@gmail.com>
2023-12-27 15:59:52 +0100

    correct  user login, signup and reset passwords templates

--- rent_flat/rent_flat/settings.py
+++ rent_flat/rent_flat/settings.py
@@ -142,3 +142,11 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 USE_JQUERY_DJANGO = True
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
