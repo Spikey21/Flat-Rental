cf7e47223c31633a666669ae55481bdacf444a84
Spikey <rafal.paciorek19@gmail.com>
2024-01-26 00:12:00 +0100

    add change password done view

--- rent_flat/authSystem/urls.py
+++ rent_flat/authSystem/urls.py
@@ -6,11 +6,12 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LogIn.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.LogOut.as_view(), name='logout'),
     path("signup/", views.SignUp.as_view(), name="signup"),
     path('account/', views.UserView.as_view(), name='profile'),
-    path("account/change_password/", views.PasswordChange.as_view(), name="change_password"),
-    path('account/reset_password/', views.ResetPasswordView.as_view(), name='reset_password'),
+    path("account/change-password/", views.PasswordChange.as_view(), name="change_password"),
+    path('account/change-password/done/', views.PasswordChangeDone.as_view(), name='change_password_done'),
+    path('account/reset-password/', views.ResetPasswordView.as_view(), name='reset_password'),
     path('account/reset-password-confirm/<uidb64>/<token>/',
          views.ResetConfirmPasswordView.as_view(),
          name='reset_password_confirm'),
