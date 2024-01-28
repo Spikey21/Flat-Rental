4c07bfc15de54bd28a299c50d5ea9714057ca50f
Spikey <rafal.paciorek19@gmail.com>
2024-01-29 00:04:28 +0100

    create notify view

--- rent_flat/notify/views.py
+++ rent_flat/notify/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from notifications.models import Notification
 
-# Create your views here.
+@login_required
+def notifications_view(request):
+    notifications = Notification.objects.filter(recipient=request.user)
+    return render(request, 'notifications.html', {'notifications': notifications})
