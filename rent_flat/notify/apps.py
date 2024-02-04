25d8e33be08d65fea04ee166ceab7eb511c3afdf
Spikey <rafal.paciorek19@gmail.com>
2024-02-05 00:36:42 +0100

    create notify signal

--- rent_flat/notify/apps.py
+++ rent_flat/notify/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class NotifyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'notify'
+
+    def ready(self):
+        try:
+            import notify.signals
+        except ImportError:
+            pass
---
+++ rent_flat/notify/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from notifications.signals import notify
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+
+from flat.models import Flat
+from .models import Notification, NotificationType
+
+
+@receiver(post_save, sender=Flat)
+def create_flat(sender, created, instance, update_fields, **kwargs):
+    if created:
+        notify.send(Flat, recipient=instance.user, verb=_("You have new flat"))
