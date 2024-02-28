9fd39957e896148ea011ebdb8fc396ca49290ae2
Spikey <rafal.paciorek19@gmail.com>
2024-02-29 00:57:30 +0100

    add urls to base temp

--- rent_flat/flat/urls.py
+++ rent_flat/flat/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.HomeListView.as_view(), name='home'),
     path('create/', views.FlatCreateView.as_view(), name='create'),
     path('flats/', views.FlatListView.as_view(), name='flats'),
-    path('my-ads/', views.MyFlatListView.as_view(), name='my_flats'),
+    path('my-ads/', views.MyFlatListView.as_view(), name='my_ads'),
     path('flat/<int:pk>/', views.FlatDetailView.as_view(), name='detail'),
     path('update-flat/<int:pk>/', views.FlatUpdateView.as_view(), name='update'),
     path('chaining/', include('smart_selects.urls')),
