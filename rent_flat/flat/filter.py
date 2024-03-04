cb4329ff19775875f05e5615ed5dd75c8b6e63ac
Spikey <rafal.paciorek19@gmail.com>
2024-03-05 00:34:06 +0100

    migrate refactor model

--- rent_flat/flat/filter.py
+++ rent_flat/flat/filter.py
@@ -2,16 +2,16 @@ import django_filters
 from django import forms
 from django_filters import RangeFilter, MultipleChoiceFilter, ModelMultipleChoiceFilter, ChoiceFilter, ModelChoiceFilter
 
-from .models import Flat, Room, FlatLocation
+from .models import Flat, FlatLocation, FlatDetail
 
 
 class FlatFilter(django_filters.FilterSet):
     price = RangeFilter()
     area = RangeFilter()
-    rooms = ModelMultipleChoiceFilter(queryset=Room.objects.all(), widget=forms.CheckboxSelectMultiple())
+    detail = ModelChoiceFilter(queryset=FlatDetail.objects.all(), label='FlatDetail', distinct=True)
     year = RangeFilter()
     location = ModelChoiceFilter(queryset=FlatLocation.objects.all(), label='FlatLocation', distinct=True)
 
     class Meta:
         model = Flat
-        fields = ['price', 'area', 'rooms', 'development_type', 'year', 'location__city']
+        fields = ['price', 'area', 'detail__rooms', 'detail__development_type', 'year', 'location__city']
