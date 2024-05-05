464cba40a33f298982927341d0667dd7fa2b8732
Spikey21 <rafal.paciorek19@gmail.com>
2024-05-05 23:34:29 +0200

    add notify detail temp

--- rent_flat/notify/urls.py
+++ rent_flat/notify/urls.py
@@ -5,4 +5,5 @@ import notifications.urls
 
 urlpatterns = [
     path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    path('inbox/notifications/<int:pk>/detail', views.NotificationDetailView.as_view(), name='notification_detail'),
 ]
--- rent_flat/notify/views.py
+++ rent_flat/notify/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView, ListView
 
 from .models import Notification
