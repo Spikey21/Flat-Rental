3d19f84b6f614a879ccf2a9cc7a3df1cbff30f75
Spikey21 <rafal.paciorek19@gmail.com>
2024-06-06 23:59:56 +0200

    change in signal noti

--- rent_flat/notify/signals.py
+++ rent_flat/notify/signals.py
@@ -1,5 +1,5 @@
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from notifications.signals import notify
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
@@ -13,6 +13,17 @@ def create_flat(sender, created, instance, update_fields, **kwargs):
     if created:
         notify.send(instance, recipient=instance.user, notification_type=NotificationType.objects.filter(name="Add").first(), verb=_("New flat showed up!"), message=_("You have new flat in your preferences"))
 
+
+@receiver(pre_save, sender=Flat)
+def on_change(sender, instance, **kwargs):
+    if instance.id is None: # new object will be created
+        pass # write your code here
+    else:
+        previous = Flat.objects.get(id=instance.id)
+        if previous.title != instance.title: # field will be updated
+            notify.send(instance, recipient=instance.user,
+                        notification_type=NotificationType.objects.filter(name=("Del" or "Mod")).first(),
+                        verb=_("Status od flat has changed"), message=_("Status has changed of your prefereneces"))
 
 @receiver(post_save, sender=Flat)
 def update_flat(sender, created, instance, update_fields, **kwargs):
