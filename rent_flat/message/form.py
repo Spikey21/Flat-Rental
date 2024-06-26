4440300801957ceb89d9be183a0b7ebcc524084d
Spikey21 <rafal.paciorek19@gmail.com>
2024-06-26 20:19:01 +0200

    correct chat and message form

--- rent_flat/message/form.py
+++ rent_flat/message/form.py
@@ -5,15 +5,17 @@ from .models import Message
 class ChatForm(forms.ModelForm):
     class Meta:
         model = Message
-        fields = ['participants',]
+        fields = ['name','participants']
 
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
-        fields = ['chat', 'text']
+        fields = ['text',]
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({'placeholder': 'Type your message...'})
         super(MessageForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
--- rent_flat/message/models.py
+++ rent_flat/message/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Chat(models.Model):
-    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True, null=True)
     participants = models.ManyToManyField(User, related_name='chats')
     created = models.DateTimeField(auto_now_add=True)
 
