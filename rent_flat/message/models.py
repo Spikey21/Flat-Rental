3ef00ffdc0145e5169627fd19471693a93bc4538
Spikey21 <rafal.paciorek19@gmail.com>
2024-06-26 19:15:11 +0200

    add chat and message model

--- rent_flat/message/models.py
+++ rent_flat/message/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, related_name='chats')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=500)
+
