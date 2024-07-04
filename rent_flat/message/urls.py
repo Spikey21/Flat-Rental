08f0282724dac803a675e7f6cc0c8f1ff1b0c5f4
Spikey21 <rafal.paciorek19@gmail.com>
2024-07-04 17:31:06 +0200

    correct urls and add link to base

--- rent_flat/message/urls.py
+++ rent_flat/message/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ChatListView, ChatCreateView, ChatDetailView, MessageCreateView
 
 urlpatterns = [
-    path('', ChatListView.as_view(), name='chat_list'),
-    path('create/', ChatCreateView.as_view(), name='create_chat'),
-    path('<int:pk>/', ChatDetailView.as_view(), name='chat_detail'),
-    path('<int:chat_id>/send/', MessageCreateView.as_view(), name='send_message'),
+    path('chat-list', ChatListView.as_view(), name='chat_list'),
+    path('chat-list/create/', ChatCreateView.as_view(), name='create_chat'),
+    path('chat-list/<int:pk>/', ChatDetailView.as_view(), name='chat_detail'),
+    path('chat-list/<int:chat_id>/send/', MessageCreateView.as_view(), name='send_message'),
 ]
